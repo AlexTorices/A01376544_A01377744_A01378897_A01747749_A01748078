25b7bc027bb79a3c58c8a8974d43301cb4e4f0e6
ClaudioMayoral <claudio_mayoral@hotmail.com>
2022-08-23 20:59:24 -0600

    Semana 3

--- Semana3/appsimple/src/index.js
+++ Semana3/appsimple/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom'; 
 import VideojuegosApp from './VideojuegosApp'; 
 import './index.css'; 
+import ListaVideojuegosApp from './ListaVideojuegosApp';
  
 const divRoot = document.querySelector('#root'); 
  
 //Utilizamos un prop 'nombre' para enviar información al componente 
 // VideojuegosApp 
-ReactDOM.render(<VideojuegosApp nombre='Jorge' />, divRoot); 
+ReactDOM.render(<ListaVideojuegosApp nombre='Jorge' />, divRoot); 
