01c6ec954e4e84e013be5bb38497ed94f0e8f6e8
AlexLeyMar <80998860+AlexLeyMar@users.noreply.github.com>
2022-09-27 21:03:03 -0500

    Arreglo Ejericicio 2

--- Semana4/consultavj/src/componentes/ResultadoVideojuegos.js
+++ Semana4/consultavj/src/componentes/ResultadoVideojuegos.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useFetch } from '../componentes/useFetch';
 import { InfoVideojuegos } from './infoVideojuegos';
 
 //Recibe como argumento el género que se va utilizar para hacer la búsqueda de los videojuegos
@@ -13,6 +14,10 @@ export const ResultadoVideojuegos = ({ genero }) => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    //Utilizamos el custom hook 'useFetch'. Desestructuramos la información que devuelve el custom hook.
+    const { loading, info } = useFetch('https://api.rawg.io/api/games?key=88de3d5868fa451faafb8012c63acda6&genres=' + genero); 
+    console.log({loading, info});
+
     //Invocamos el api de RAWG para obtener los videojuegos del género proporcionado en los parámetrs del
     // componente.
     const getVideojuegos = async () => {
@@ -21,7 +26,7 @@ export const ResultadoVideojuegos = ({ genero }) => {
         //Utilizamos Fetch API para invocar la url.
         const respuesta = await fetch(url);
         //Recuperamos el JSON de la respuesta, el cual contiene la información de los videojuegos.
-        const { results } = await respuesta.json();
+        const {results} = await respuesta.json();
         //Obtenemos solamente la información que vamos a necesitar del json de la respuesta.
         const juegos = results.map(juego => {
             return {
@@ -38,15 +43,32 @@ export const ResultadoVideojuegos = ({ genero }) => {
         setInfoJuegos(juegos);
     }
 
-
     return (
         <>
-            <h3 className="card-title">{genero}</h3>
-            {/*
-            Creamos la lista de juegos con la información que recuperamos de la invocación del api de RAWG,
-            utilizando la variable infoJuegos que obtuvimos en la desestructuración del hook useState.
-            */}
-            <InfoVideojuegos infoJuegos={infoJuegos}/>
+        <h1>Información de Videojuegos</h1> <hr />
+        {
+            loading
+                ? 
+                (
+                    //Si loading es TRUE, mostramos el mensaje 'loading'
+                    <div className='alert alert-info text-center card w-25'>
+                        Loading...
+                    </div>
+                )
+                :
+                (
+                    <>
+                        <h3 className="card-title">{genero}</h3>
+                        {/*
+                        Creamos la lista de juegos con la información que recuperamos de la invocación del api de RAWG,
+                        utilizando la variable infoJuegos que obtuvimos en la desestructuración del hook useState.
+                        */}
+                        <InfoVideojuegos infoJuegos={infoJuegos}/>
+                    </>
+                )
+        }
+            
         </>
-    )
+    );
+
 }
--- Semana4/consultavj/src/index.js
+++ Semana4/consultavj/src/index.js
@@ -8,4 +8,6 @@ const container = document.getElementById('root');
 
 const root = createRoot(container);
 
+//root.render(<VideojuegosApp />);
+
 root.render(<MultipleCustomHooks />);
